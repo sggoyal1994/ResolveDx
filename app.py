982d5b86c5746ce4dd2d75993985ee4fac92d7de
Rajat <rajatb@deloitte.com>
2024-06-17 22:06:51 +0530

    Adding Logger

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, request
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 import query_response
+import logging
 
 DOMAINS_ALLOWED = "*"  # You can restrict only for your sites here
 app = Flask(__name__)
@@ -15,6 +16,13 @@ cors = CORS(app, resources={r"/*": {"origins": DOMAINS_ALLOWED}})
 # Get the environmental variables
 app.config["DEBUG"] = os.environ.get("FLASK_DEBUG")
 bcrypt = Bcrypt(app)
+
+# Set up logging
+logging.basicConfig(filename='flask_app.log',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+# Create a logger object
+logger = logging.getLogger(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -26,6 +34,8 @@ def home():
         str: A message with the Python version.
     """
     message = "Python version: " + sys.version
+    app.logger.info('Home endpoint was reached')
+    logger.info('Home endpoint was reached using custom logger')
     return message
 
 
@@ -41,11 +51,15 @@ def query_response():
         str: The response from the avatar service.
     """
     try:
+        logger.info("Inside getQueryResponse API--------------------------")
+
         data = request.json
+        logger.info(data)
         input_text = data['query']
         session_id = data.get()('session_id', uuid.uuid4())
         if input_text != '':
             response = query_response.process_query(input_text)
+            logger.info("Response", response)
             response_data = {"message": response, "session_id": session_id}
             return jsonify({'response': response_data, "success": True}), 200
         else:
