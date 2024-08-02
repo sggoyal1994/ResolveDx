7bf28ce3f4e6b3a5f73859118f6c52d8ab1db846
Shilpa Garg <32759884+sggoyal1994@users.noreply.github.com>
2024-08-02 12:01:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# AWS access credentials
+access_key = ''
+secret_key = ''
+GROQ_API_KEY = "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"
+# S3 bucket details
+bucket_name = 'sentinelx-prod'
+prefix = 'LOTO/Documents/LOTOFormDocuments/'
+MODEL_NAME = "sentence-transformers/all-mpnet-base-v2"
